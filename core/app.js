2894e29e125cef4ca3cfb66e25a5ff2e919b59d7
Paul Harper <benekastah@gmail.com>
2011-07-20 23:07:22 -0600

    Working

--- core/app.js
+++
@@ -1,5 +0,0 @@
-// This file just runs everything else. Any scripts to be kaffeinated should be required here.
-// This is also the entry point to the application.
-
-require("kaffeine");
-require("./server");
